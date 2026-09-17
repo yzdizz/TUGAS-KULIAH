a11533d215a3a51d170f26d87d2617cb08f5f016
yzdizz <muhammadnizambiyazid@gmail.com>
2026-09-17 20:36:04 +0800

    Create Variable003.java

---
+++ Variable003.java
@@ -0,0 +1,26 @@
+public class Variable003 {
+    public static void main(String[] args) {
+        // deklarasi variable
+        long nim1, nim2;
+        String nama1, nama2, garis;
+
+        // isi variabel
+        nim1 = 2611016300000L;
+        nim2 = 2611016100000L;
+        nama1 = "BUDI";
+        nama2 = "AGUS";
+        garis = "==========================================";
+
+        // cetak variable
+        System.out.println(garis);
+        System.out.println("NIM            |  Nama");
+        System.out.println(garis);
+        System.out.print(nim1);
+        System.out.print("  |  ");
+        System.out.println(nama1);
+        System.out.print(nim2);
+        System.out.print("  |  ");
+        System.out.println(nama2);
+
+    }
+}
